d10b56448142b76c924b07ed1f3ccd5db38260da
JAIMIE LOWE <jaimielowe@MacBook-Pro-4.lan1>
2019-11-07 01:42:41 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,19 @@
-console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[3];
-cardsInPlay.push(cardOne);
+function checkForMatch() {
+  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+};
 
-console.log("User flipped " + cardOne);
+function flipCard(cardId) {
+  checkForMatch()
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[3] === cardsInPlay[2]) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again.");
-}
+flipCard(0);
+flipCard(2);
